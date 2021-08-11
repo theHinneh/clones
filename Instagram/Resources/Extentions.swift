a6d3b34bc2c247022fe4385a31eca91c7ec107a4
theHinneh <hinneh04@gmail.com>
2021-08-11 23:53:34 +0000

    login page

---
+++ Instagram/Resources/Extentions.swift
@@ -0,0 +1,34 @@
+//
+//  Extentions.swift
+//  Instagram
+//
+//  Created by theHinneh on 11/08/2021.
+//
+
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+}
