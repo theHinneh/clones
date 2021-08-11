c3455b09668d989ab09854e06cc5feb62215cece
theHinneh <hinneh04@gmail.com>
2021-08-11 17:01:59 +0000

    check in autorized

--- Instagram/Controllers/Other/Onboarding/LoginViewController.swift
+++ Instagram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -11,8 +11,8 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
 
 }
