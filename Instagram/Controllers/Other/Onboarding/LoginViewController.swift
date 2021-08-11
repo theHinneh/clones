61bfd813f3cb95ae6fc37131a7c112ccc0976e05
theHinneh <hinneh04@gmail.com>
2021-08-11 17:28:13 +0000

    firebase setup

--- Instagram/Controllers/Other/Onboarding/LoginViewController.swift
+++ Instagram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -8,11 +8,65 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let usernameEmailField: UITextField = {
+        return UITextField()
+    }()
+    
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let termsButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let privacyButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let createAccountButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let headerView: UIView = {
+        return UIView()
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubviews()
+        view.backgroundColor = .systemBackground
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
-        view.backgroundColor = .red
+        // assign framaes
+        
     }
 
+    private func addSubviews() {
+        view.addSubview(usernameEmailField)
+        view.addSubview(passwordField)
+        view.addSubview(loginButton)
+        view.addSubview(termsButton)
+        view.addSubview(privacyButton)
+        view.addSubview(headerView)
+        view.addSubview(createAccountButton)
+    }
+    
+    @objc private func didTapLoginButton() {}
+    
+    @objc private func didTapTermsButton() {}
+    
+    @objc private func didTapPrivacyButton() {}
+    
+    @objc private func didTapCreateAccountButton() {}
 }
---
+++ Instagram/Resources/AuthManager.swift
@@ -0,0 +1,20 @@
+//
+//  AuthManager.swift
+//  Instagram
+//
+//  Created by theHinneh on 11/08/2021.
+//
+
+import FirebaseAuth
+
+public class AuthManager {
+    static let shared = AuthManager()
+    
+    public func registerUser(username: String, email: String, password: String) {
+        
+    }
+    
+    public func loginUser(username: String?, email: String?, password: String) {
+        
+    }
+}
